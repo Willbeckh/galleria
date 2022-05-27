bb0551668b93ac99dc36d7429fc783b63dd2f103
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-28 02:51:12 +0300

    pt: add support for static images

--- galleria_project/urls.py
+++ galleria_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # local app import
 from . import views
@@ -22,5 +24,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('picasso/', include('picasso.urls'))
-]
+    path('picasso/', include('picasso.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
