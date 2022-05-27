368f42600abeb31f064af5427e86847a46f4897d
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-28 02:50:15 +0300

    ft: add db query for images

--- picasso/views.py
+++ picasso/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+# LOCAL IMPORTS
+from picasso.models import Image
+
 # Create your views here.
 def index(request):
-    return render(request, 'picasso/index.html')
+    images = Image.objects.all()
+    context = {
+        'images': images,
+        'title': 'Home'
+    }
+    return render(request, 'picasso/index.html', context)
