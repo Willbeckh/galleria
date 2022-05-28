c5a0a6be808fbbeef128b52741d1d78623129491
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-28 13:03:40 +0300

    pt: add tests-- delete image method && update_image method

--- picasso/tests.py
+++ picasso/tests.py
@@ -10,7 +10,8 @@ class TestImageModel(TestCase):
 
     def setUp(self):
         """Method to create the test data"""
-        self.image = Image(image_name='subaru', image_description='subaru ya mambaru')
+        self.image = Image(image_name='subaru',
+                           image_description='subaru ya mambaru')
 
     def test_instance(self):
         """Method to test the instance of the image model"""
@@ -19,5 +20,19 @@ class TestImageModel(TestCase):
     def test_save_image(self):
         """Method to test the save image method"""
         self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        """Method to test the delete image method"""
+        self.image.save_image()
+        self.image.delete_selected_image(self.image.id)
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+        
+    def test_update_image(self):
+        """Method to test the update image method"""
+        self.image.save_image()
+        self.image.update_image(self.image.id, 'subaru STI', 'subaru ni mpya')
         images = Image.objects.all()
         self.assertTrue(len(images) > 0)
