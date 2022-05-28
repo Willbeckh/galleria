57972a804821f6d55dd2d780e6b109bf9c8b979a
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-28 21:51:18 +0300

    add search by category method ; add Category models

--- picasso/models.py
+++ picasso/models.py
@@ -19,19 +19,32 @@ class Image(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-
     def save_image(self):
         '''method to save the selected image'''
         self.save()
-        
+
     def delete_selected_image(self, id):
         '''method to delete the selected image'''
         try:
             self.image = Image.objects.filter(pk=id).delete()
         except Exception as e:
             print('Error: ', e)
-    
-    def update_image(self, id, image_name, image_description):
+
+    def update_image(self, image_name, image_description):
         '''method to update the selected image'''
-        self.image = Image.objects.filter(pk=id).update(image_name=image_name, image_description=image_description)
-        self.save_image()
+        self.image = Image.objects.filter(id=self.id).update(
+            image_name=image_name, image_description=image_description)
+
+    def search_image_by_category(self, category):
+        '''method to search the image by category'''
+        images = Image.objects.filter(image_name__icontains=category)
+        return images
+
+
+class Category(models.Model):
+    '''This Class defines the category model'''
+    categories = models.ManyToManyField(Image)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Name: {self.name}"
