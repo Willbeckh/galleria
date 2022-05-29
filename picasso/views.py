4c9b73ff13f0da9aa401c0fa628e352ca34c4b0e
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-29 22:55:46 +0300

    ft: create search filter method

--- picasso/views.py
+++ picasso/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from picasso.models import Image, Category, Location
 
 # Create your views here.
+
+
 def index(request):
     images = Image.objects.order_by('-pub_date')[:5]
     categories = Category.objects.all()
@@ -22,3 +24,24 @@ def about(request):
         'title': 'About'
     }
     return render(request, 'picasso/about.html', context)
+
+
+def search_results(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+
+    if 'search_term' in request.GET and request.GET['search_term']:
+        search_query = request.GET.get('search_term')
+        searched_images = Image.search_image_by_category(
+            '', search_query) or Image.filter_image_by_location('', search_query)
+        message = f'{search_query}'
+        context = {
+            'images': searched_images,
+            'categories': categories,
+            'title': 'Search Results',
+            'message': message
+        }
+        return render(request, 'picasso/index.html', context)
+    else:
+        message = 'No search term found'
+        return render(request, 'picasso/index.html', {'message': message})
