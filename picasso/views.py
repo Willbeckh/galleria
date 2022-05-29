e76e2866407a2ab05321b51e8fc4bb124e0b2c80
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-29 21:14:47 +0300

    add locations query on index view method

--- picasso/views.py
+++ picasso/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 # LOCAL IMPORTS
-from picasso.models import Image
+from picasso.models import Image, Category, Location
 
 # Create your views here.
 def index(request):
     images = Image.objects.order_by('-pub_date')[:5]
+    categories = Category.objects.all()
+    location = Location.objects.all()
     context = {
         'images': images,
+        'categories': categories,
+        'location': location,
         'title': 'Home'
     }
     return render(request, 'picasso/index.html', context)
